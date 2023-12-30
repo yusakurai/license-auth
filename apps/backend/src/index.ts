d39b9595a43259203ca96f184131d4ff8877c838
Sakurai Yuki <sakurai0803@gmail.com>
2023-12-30 19:22:48 +0900

    ✨ Fastify追加

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,23 @@
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: false,
+})
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
