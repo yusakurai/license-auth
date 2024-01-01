5ef99c7e80e6028efed881be7041385d13a41d7f
Sakurai Yuki <sakurai0803@gmail.com>
2024-01-01 15:13:10 +0900

    🔧 Vitest 追加

---
+++ apps/frontend/src/test/sum.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest'
+
+import { sum } from '../utils/sum'
+
+test('adds 1 + 2 to equal 3', () => {
+  // sum関数の引数に1,2を渡している
+  // 1 + 2 が 3 になるので、3という値が返ってくることを期待している
+  expect(sum(1, 2)).toBe(3)
+})
---
+++ apps/frontend/src/utils/sum.ts
@@ -0,0 +1,2 @@
+/// aとbを足し算すると関数
+export const sum = (a: number, b: number) => a + b
