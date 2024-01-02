0aa20dfcc6862cf55ea8d2d6c8bd1511556ee166
Sakurai Yuki <sakurai0803@gmail.com>
2024-01-02 14:13:42 +0900

    🔧 ui - コンポーネントテスト 追加

---
+++ packages/ui/src/Button/__tests__/Button.test.tsx
@@ -0,0 +1,37 @@
+import { render, fireEvent } from '@testing-library/react'
+import { describe, expect, test, vi } from 'vitest'
+
+import { Button } from '../Button'
+
+describe('Button', () => {
+  describe('Rendering', () => {
+    test('レンダリングされるか', () => {
+      const { getByText } = render(<Button>Button</Button>)
+
+      expect(getByText('Button')).toBeDefined()
+    })
+  })
+
+  // describe('Props', () => {
+  //   test('asChildがtrueの場合、button要素がレンダリングされないか', () => {
+  //     const { container } = render(
+  //       <Button asChild>
+  //         <a href="https://example.com/">example.com</a>
+  //       </Button>
+  //     )
+
+  //     expect(container.querySelector('button')).toBeNull()
+  //   })
+  // })
+
+  describe('Events', () => {
+    test('クリックされるか', () => {
+      const handleClick = vi.fn()
+      const { getByText } = render(<Button onClick={handleClick}>Button</Button>)
+
+      fireEvent.click(getByText('Button'))
+
+      expect(handleClick).toHaveBeenCalled()
+    })
+  })
+})
