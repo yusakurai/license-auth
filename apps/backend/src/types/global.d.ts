05c61757afb8e2c4039d244bebb1f79391075d9f
Sakurai Yuki <sakurai0803@gmail.com>
2024-01-08 08:48:55 +0900

    🐛 FirebaseType設定漏れ

--- apps/backend/src/types/global.d.ts
+++ apps/backend/src/types/global.d.ts
@@ -5,14 +5,6 @@ declare namespace NodeJS {
     readonly NODE_ENV: 'development' | 'testing' | 'production'
     readonly PORT: number
     readonly GRAPHQL_PATH: string
-
-    // Firebase
-    readonly FIREBASE_API_KEY: string
-    readonly FIREBASE_AUTH_DOMAIN: string
-    readonly FIREBASE_PROJECT_ID: string
-    readonly FIREBASE_STORAGE_BUCKET: string
-    readonly FIREBASE_MESSAGING_SENDER_ID: string
-    readonly FIREBASE_APP_ID: string
-    readonly FIREBASE_MEASUREMENT_ID: string
+    readonly FIREBASE_CREDENTIAL_FILE_NAME: string
   }
 }
