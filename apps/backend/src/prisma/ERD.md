e06409d770a9901695a829aec6796fe3a30858a0
Sakurai Yuki <sakurai0803@gmail.com>
2024-01-23 17:52:22 +0900

    ✨ PrismaのシードをFakerから作成

--- apps/backend/src/prisma/ERD.md
+++ apps/backend/src/prisma/ERD.md
@@ -1,12 +1,36 @@
 ```mermaid
 erDiagram
 
+        Gender {
+            Male Male
+Female Female
+Other Other
+        }
+    
+
+
+        Locale {
+            Ja Ja
+En En
+        }
+    
   "User" {
+    String id "🗝️"
     DateTime createdAt 
     DateTime updatedAt 
-    String id "🗝️"
     String name 
+    String nameKana 
     String email 
+    DateTime birthday 
+    Gender gender 
+    Locale locale 
+    String zipCode 
+    String address1 
+    String address2 
+    String phoneNumber 
+    String fcmtoken "❓"
     }
   
+    "User" o|--|| "Gender" : "enum:gender"
+    "User" o|--|| "Locale" : "enum:locale"
 ```
--- apps/backend/src/prisma/seed-data/user.ts
+++ apps/backend/src/prisma/seed-data/user.ts
@@ -1,19 +1,37 @@
+import { faker } from '@faker-js/faker'
+import { v4 } from 'uuid'
+
 import type { Prisma, PrismaClient } from '@prisma/client'
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: 'hoge1',
-    email: 'hoge1@example.com',
-  },
-  {
-    name: 'hoge2',
-    email: 'hoge2@example.com',
-  },
-  {
-    name: 'hoge3',
-    email: 'hoge3@example.com',
-  },
-]
+const capitalizeFirstLetter = <T>(value: string): T => {
+  return (value.charAt(0).toUpperCase() + value.slice(1)) as T
+}
+
+const generateUsers = (): Prisma.UserCreateInput[] => {
+  const users = []
+
+  for (let i = 0; i < 100; i++) {
+    const data: Prisma.UserCreateInput = {
+      id: v4(),
+      name: faker.person.fullName(),
+      nameKana: faker.person.middleName(),
+      email: faker.internet.email(),
+      birthday: faker.date.birthdate(),
+      gender: capitalizeFirstLetter<Prisma.UserCreateInput['gender']>('male'),
+      locale: capitalizeFirstLetter<Prisma.UserCreateInput['locale']>('ja'),
+      zipCode: faker.location.zipCode(),
+      address1: faker.location.streetAddress({ useFullAddress: true }),
+      address2: faker.location.secondaryAddress(),
+      phoneNumber: faker.helpers.fromRegExp('090-0[0-9]{3}-[0-9]{4}'),
+    }
+
+    users.push(data)
+  }
+
+  return users
+}
+
+const userData: Prisma.UserCreateInput[] = generateUsers()
 
 export const createUsers = async (prisma: PrismaClient) => {
   const users = []
