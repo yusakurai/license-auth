d630eb24019aeb6510c13ac1f2855144bf2f2758
Sakurai Yuki <sakurai.yuki@nck.co.jp>
2024-01-31 14:55:58 +0900

    🔧 @storybook/addon-designs追加

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -17,6 +17,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-designs'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
