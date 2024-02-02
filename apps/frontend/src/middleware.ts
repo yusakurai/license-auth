b0d560657534997222f9641406fbb1cd1b3adbaa
Sakurai Yuki <sakurai.yuki@nck.co.jp>
2024-02-02 14:24:45 +0900

    ✨ 未ログイン時のリダイレクト処理追加

---
+++ apps/frontend/src/middleware.ts
@@ -0,0 +1,10 @@
+// middleware.ts
+import { getSession, withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge'
+import { NextResponse } from 'next/server'
+
+export default withMiddlewareAuthRequired(async function middleware(req) {
+  const res = NextResponse.next()
+  const user = await getSession(req, res)
+  console.log('user:', user?.user)
+  return res
+})
